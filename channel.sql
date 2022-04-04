d9b187e9fff5513fd0ef0e13723fc017ddf98efe
Anatoly Olennikov <a.olennikov@iqdev.digital>
2022-04-05 00:14:34 +0500

    add migration scheme

---
+++ channel.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 04, 2022 at 10:13 PM
+-- Server version: 8.0.24
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `slack`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel`
+--
+
+CREATE TABLE `channel` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `id_workflow` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_workflow` (`id_workflow`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `channel`
+--
+ALTER TABLE `channel`
+  ADD CONSTRAINT `channel_ibfk_1` FOREIGN KEY (`id_workflow`) REFERENCES `workflow` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
