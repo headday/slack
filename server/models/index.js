315faa04731a75138eefae16cea796603a1ce7c1
Kirll <k.svezhinov@iqdev.digital>
2022-04-05 10:28:25 +0500

    fix

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,7 @@
 const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 
-const User = sequelize.define('user', {
+const User = sequelize.define('users', {
         id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
         name: {type: DataTypes.STRING},
         login: {type: DataTypes.STRING},
